bd31a1f3eba5b259729b207712fedf65f6c595c1
Minkyung Song <msong320@gatech.edu>
2023-02-11 17:00:13 -0500

    added setting for difficulty button

--- app/src/main/java/com/example/crosstheroad/SelectFragment.java
+++ app/src/main/java/com/example/crosstheroad/SelectFragment.java
@@ -31,6 +31,9 @@ public class SelectFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.buttonEasy.setOnClickListener(view1 -> lives = 7);
+        binding.buttonNormal.setOnClickListener(view1 -> lives = 5);
+        binding.buttonHard.setOnClickListener(view1 -> lives = 3);
         binding.buttonSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
