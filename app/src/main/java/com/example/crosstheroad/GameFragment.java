1a03b4f156fae637a152cce1c683404cbc6ba5c4
wchoi321 <wchoi321@gatech.edu>
2023-02-13 21:07:10 -0500

    2/13 9:06, Wonjae Choi

--- app/src/main/java/com/example/crosstheroad/GameFragment.java
+++ app/src/main/java/com/example/crosstheroad/GameFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +16,9 @@ public class GameFragment extends Fragment {
     private int lives;
     private String name;
     private String difficulty;
+    private String sprite;
+    private int spriteInt;
+
 
     public GameFragment() {
         // Required empty public constructor
@@ -24,38 +27,57 @@ public class GameFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        score = 00000;
+        score = 0;
         lives = 0;
-        name = "asdf";
+        name = "aaaa";
         difficulty = "difficulty";
+        sprite = "a";
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Displaying the number of life
         TextView livesValue = view.findViewById(R.id.lives_value);
         lives = getArguments().getInt("lives");
-        String lives_string =String.valueOf(lives);
-        livesValue.setText(lives_string);
+        String livesString = String.valueOf(lives);
+        livesValue.setText(livesString);
 
-
-
+        // Displaying the user name
         TextView nameValue = view.findViewById(R.id.name_value);
         name = getArguments().getString("name");
         nameValue.setText(name);
 
-
+        // Displaying the difficulty
         TextView difficultyValue = view.findViewById(R.id.difficulty_value);
         difficulty = getArguments().getString("difficulty");
         difficultyValue.setText(difficulty);
+
+        // Default score
+        TextView scoreValue = view.findViewById(R.id.score_value);
+        String scoreString = String.valueOf(score);
+        scoreValue.setText(scoreString);
+
+        // Image changes depdends on user character
+        ImageView imageView = view.findViewById(R.id.userCharacter);
+        spriteInt = getArguments().getInt("spriteInt");
+
+        // frog = 0, dog = 1,cat = 2;
+        if (spriteInt == 0) {
+            imageView.setImageResource(R.drawable.selected_frog);
+        } else if (spriteInt == 1) {
+            imageView.setImageResource(R.drawable.selected_dog);
+        } else {
+            imageView.setImageResource(R.drawable.selected_cat);
+        }
+
     }
 
 }
--- app/src/main/java/com/example/crosstheroad/SelectFragment.java
+++ app/src/main/java/com/example/crosstheroad/SelectFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,9 +21,10 @@ public class SelectFragment extends Fragment {
     private String sprite;
     private String name;
     private int lives;
+    private int spriteInt;
     private String difficulty;
 
-
+    // frog = 0, dog = 1,cat = 2;
 
     Fragment fragment = new Fragment();
     @Override
@@ -29,40 +32,48 @@ public class SelectFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentSelectBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         binding.frog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // When frog is selected
                 binding.frog.setImageResource(R.drawable.selected_frog);
                 binding.dog.setImageResource(R.drawable.dog);
                 binding.cat.setImageResource(R.drawable.cat);
-
+                sprite = "frog";
+                spriteInt = 0;
             }
         });
         binding.dog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // when dog is selected
                 binding.frog.setImageResource(R.drawable.frog);
                 binding.dog.setImageResource(R.drawable.selected_dog);
                 binding.cat.setImageResource(R.drawable.cat);
+                sprite = "dog";
+                spriteInt = 1;
             }
         });
         binding.cat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // When cat is selected
                 binding.frog.setImageResource(R.drawable.frog);
                 binding.dog.setImageResource(R.drawable.dog);
                 binding.cat.setImageResource(R.drawable.selected_cat);
-
+                sprite = "cat";
+                spriteInt = 2;
             }
         });
+
+        // Difficulty with the number of life
         binding.buttonEasy.setOnClickListener(view1 -> {lives = 3;
             difficulty = "Easy";});
         binding.buttonNormal.setOnClickListener(view1 -> {lives = 5;
@@ -71,15 +82,22 @@ public class SelectFragment extends Fragment {
         binding.buttonHard.setOnClickListener(view1 -> {lives = 7;
             difficulty = "Hard";
         });
-        name = binding.Name.getText().toString();
+
+        // Get the user input
+        EditText nameInput = (EditText) view.findViewById(R.id.Name);
+
         binding.buttonSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                name = nameInput.getText().toString();
+
                 if (name != null && !name.equals(" ")) {
                     Bundle bundle = new Bundle();
                     bundle.putInt("lives", lives);
                     bundle.putString("name", name);
                     bundle.putString("difficulty", difficulty);
+                    bundle.putString("sprite", sprite);
+                    bundle.putInt("spriteInt", spriteInt);
                     NavHostFragment.findNavController(SelectFragment.this)
                             .navigate(R.id.action_SelectFragment_to_gameFragment,bundle);
                 }
